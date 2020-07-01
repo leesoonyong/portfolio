6b040e89183e73d36f616e72692dac10407622ec
leesoonyong <44168355+leesoonyong@users.noreply.github.com>
2020-07-01 16:10:55 +0900

    JS > home transparent
    JS > home transparent

--- main.js
+++ main.js
@@ -28,6 +28,12 @@ homeContactBtn.addEventListener("click", (event) => {
   scrollIntoView("#contact");
 });
 
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
