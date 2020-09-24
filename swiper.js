2508c2f47b3cb09d82419bde414186e2b73ffd61
leesoonyong <44168355+leesoonyong@users.noreply.github.com>
2020-09-24 19:55:01 +0900

    commit
    commit

---
+++ swiper.js
@@ -0,0 +1,34 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // 그룹수가 맞지 않을 경우 빈칸으로 메우기
+  // 3개가 나와야 되는데 1개만 있다면 2개는 빈칸으로 채워서 3개를 만듬
+  loopFillGroupWithBlank: true,
+
+  loop: true, // 무한 반복
+
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+
+  navigation: {
+    // 네비게이션
+    nextEl: ".swiper-button-next", // 다음 버튼 클래스명
+    prevEl: ".swiper-button-prev", // 이번 버튼 클래스명
+  },
+});
